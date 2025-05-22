6ff33be6aa4c1a054f9a124eda1ae3797a884019
RoshanJhanzaib <sirftum2550@gmail.com>
2025-05-23 00:02:53 +0500

    add spiner

--- js/main.js
+++ js/main.js
@@ -2,3 +2,10 @@ AOS.init({
     duration: 1000, 
     once: true
   });
+
+  window.addEventListener('load', function() {
+  const spinner = document.getElementById('spinner');
+  setTimeout(function() {
+    spinner.style.display = 'none';
+  }, 1000); // 2000 milliseconds = 2 seconds
+});
